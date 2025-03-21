98c277c31d30d7200a7e48a01053a491a90a4cf7
Manuel Aljama Muñoz <maljmun488@g.educaand.es>
2025-03-21 14:31:25 +0100

    examen

--- ex_html-css-js.js
+++ ex_html-css-js.js
@@ -1,1 +1,26 @@
-//Escribe aquí tu código JavaScript
+document.getElementById("cuenta").oninput = function(){
+    document.getElementById("contador").textContent = this.value.length;
+};
+document.getElementById("cambiarFondo").onclick=function(){
+    document.body.style.backgroundColor = "black";
+};
+
+
+
+  let boton = document.getElementById("boton");
+
+  boton.addEventListener("mouseover", function () {
+    boton.style.backgroundColor = "white";
+    boton.style.color = "blue";
+  });
+
+  boton.addEventListener("mouseout", function () {
+    boton.style.backgroundColor = "blue";
+    boton.style.color = "white";
+  });
+
+  let nombre = document.getElementById("nombre");
+boton.addEventListener("click", function(){
+    alert("¿Seguro que quieres enviarlo?")
+    alert("Gracias por tu pedido"+nombre)
+});
